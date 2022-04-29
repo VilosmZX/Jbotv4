aaf21400eba28e6914f5d98e3c78f637bf9639ed
jravaell <jravaell@gmail.com>
2022-04-29 18:41:15 +0700

    Added 1 new command

---
+++ commands/General/general.py
@@ -0,0 +1,41 @@
+from io import BytesIO
+import os
+import random
+from re import A
+from typing import Optional
+import dotenv
+import discord
+from discord.ext import commands
+from PIL import Image, ImageFont, ImageDraw
+from discord import app_commands
+from commands.utils import generate_time
+
+
+dotenv.load_dotenv()
+
+class General(commands.Cog):
+  def __init__(self, bot: commands.Bot):
+    self.bot = bot
+    self.fonts = os.path.join(os.getcwd(), 'fonts')
+
+
+  @app_commands.command(name='amiadmin', description='Check apakah kamu admin atau tidak')
+  async def amiadmin(self, interaction: discord.Interaction):
+    is_admin = None
+    for role in interaction.user.roles:
+      if role.permissions.ban_members or role.permissions.administrator:
+        is_admin = True
+
+    embed = discord.Embed(color=discord.Color.random())
+    if is_admin:
+      embed.description = f'{interaction.user.mention} adalah seorang admin!'
+      return await interaction.response.send_message(embed=embed)
+    embed.description = f'{interaction.user.mention} bukan seorang admin!'
+    return await interaction.response.send_message(embed=embed)
+
+
+
+
+
+async def setup(bot: commands.Bot):
+  await bot.add_cog(General(bot), guilds=[discord.Object(id=os.environ.get('GUID'))])
