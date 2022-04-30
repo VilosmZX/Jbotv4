4ac34dfbc8ec074f052fb8e27d772e9b04e0f2f6
jravaell <jravaell@gmail.com>
2022-04-30 09:11:00 +0700

    Fix bug and added 2 new command

--- commands/Moderation/role.py
+++ commands/Moderation/role.py
@@ -16,29 +16,49 @@ class Lock(commands.Cog, app_commands.Group, name = 'lock'):
   @app_commands.checks.has_permissions(manage_roles=True)
   async def give_role(self, interaction: discord.Interaction, user: discord.Member, role: discord.Role):
     embed = discord.Embed(color = discord.Color.random())
+    if user.guild_permissions.administrator:
+      return await interaction.response.send_message(f'{user} adalah seorang administrator')
     if user.get_role(role.id) == None:
       embed.description = f'{user.mention} sudah memiliki role {role}'
       return await interaction.response.send_message(embed=embed)
     embed.description = f'Memberikan role {role} kepada {user}'
-    await user.add_roles(role)
+    await user.add_roles([role])
     await interaction.response.send_message(embed=embed)
       
-  @give_role.error 
-  async def on_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError) -> None:
-      if isinstance(error, app_commands.MissingPermissions):
-        return await interaction.response.send_message('Tidak ada akses', ephemeral=True)
       
   @app_commands.command(name = 'remove', description='Mengambil role dari user')
   @app_commands.checks.has_permissions(manage_roles=True)
   async def remove_role(self, interaction: discord.Interaction, user: discord.Member, role: discord.Role):
     embed = discord.Embed(color = discord.Color.random())
+    if user.guild_permissions.administrator:
+      return await interaction.response.send_message(f'{user} adalah seorang administrator')
     if user.get_role(role.id) == None:
       embed.description = f'{user.mention} tidak memiliki role {role}'
       return await interaction.response.send_message(embed=embed)
     embed.description = f'Mengambil role {role} dari {user.mention}'
-    await user.remove_roles(role)
+    await user.remove_roles([role])
     await interaction.response.send_message(embed=embed)
     
+  @app_commands.command(name = 'clear', description='Mengambil semua role dari user')
+  @app_commands.checks.has_permissions(manage_roles=True)
+  async def clear_roles(self, interaction: discord.Interaction, user: discord.Member):
+    embed = discord.Embed(color = discord.Color.random())
+    if user.guild_permissions.administrator:
+      return await interaction.response.send_message(f'{user} adalah seorang administrator')
+    if len(user.roles) == 0:
+      return await interaction.response.send_message(f'{user} tidak memiliki role apapun')
+    embed.description = f'Mengambil {len(user.roles)} role dari {user.mention}'
+    await user.remove_roles(user.roles)
+    await interaction.response.send_message(embed=embed)
+    
+  @give_role.error 
+  async def on_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError) -> None:
+      if isinstance(error, app_commands.MissingPermissions):
+        return await interaction.response.send_message('Tidak ada akses', ephemeral=True)
+      
+  
+    
+  
 
 async def setup(bot: commands.Bot):
   await bot.add_cog(Lock(bot), guilds=[discord.Object(id=os.environ.get('GUID'))])
