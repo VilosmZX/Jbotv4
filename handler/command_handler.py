9b1e444fd1aaaa615f6084f141b0744a6744d034
jravaell <jravaell@gmail.com>
2022-04-30 13:44:09 +0700

    add new exception file

--- handler/command_handler.py
+++ handler/command_handler.py
@@ -2,7 +2,7 @@ import os
 from discord.ext import commands 
 
 async def load_all(bot: commands.Bot):
-  except_files = ['utils.py', '__init__.py', 'timestamp.py']
+  except_files = ['utils.py', '__init__.py', 'timestamp.py', 'checkwarncollection.py']
   for category in os.listdir(os.path.join(os.getcwd(), 'commands')):
     for command in os.listdir(os.path.join(os.getcwd(), 'commands', category)):
       if command.endswith('.py') and command not in except_files:
