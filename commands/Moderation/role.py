17a791b2912c7ad585714c1091f04c8b7b89b790
jravaell <jravaell@gmail.com>
2022-04-30 09:05:55 +0700

    Add /remove role command

--- commands/Moderation/role.py
+++ commands/Moderation/role.py
@@ -15,7 +15,6 @@ class Lock(commands.Cog, app_commands.Group, name = 'lock'):
   @app_commands.command(name = 'give', description='Memberikan role kepada user')
   @app_commands.checks.has_permissions(manage_roles=True)
   async def give_role(self, interaction: discord.Interaction, user: discord.Member, role: discord.Role):
-    
     embed = discord.Embed(color = discord.Color.random())
     if user.get_role(role.id) == None:
       embed.description = f'{user.mention} sudah memiliki role {role}'
@@ -28,6 +27,19 @@ class Lock(commands.Cog, app_commands.Group, name = 'lock'):
   async def on_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError) -> None:
       if isinstance(error, app_commands.MissingPermissions):
         return await interaction.response.send_message('Tidak ada akses', ephemeral=True)
+      
+  @app_commands.command(name = 'remove', description='Mengambil role dari user')
+  @app_commands.checks.has_permissions(manage_roles=True)
+  async def remove_role(self, interaction: discord.Interaction, user: discord.Member, role: discord.Role):
+    embed = discord.Embed(color = discord.Color.random())
+    if user.get_role(role.id) == None:
+      embed.description = f'{user.mention} tidak memiliki role {role}'
+      return await interaction.response.send_message(embed=embed)
+    embed.description = f'Mengambil role {role} dari {user.mention}'
+    await user.remove_roles(role)
+    await interaction.response.send_message(embed=embed)
+    
 
 async def setup(bot: commands.Bot):
   await bot.add_cog(Lock(bot), guilds=[discord.Object(id=os.environ.get('GUID'))])
+  
